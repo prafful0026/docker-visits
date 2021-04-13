7e57bd2a9d7192f2272baef39408bd7c5d890038
prafful0026 <prafful0026@gmail.com>
2021-04-13 22:22:54 +0530

    configured docker-compose file so that container restarts itself in case of faliure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 const redis=require("redis")
-
+const process =require("process")
+ 
 const app=express()
 const client=redis.createClient({
     host: "redis-server"
@@ -10,8 +11,8 @@ client.set("visits",0)
 
 
 app.get("/",(req,res)=>{
-
-    client.get("visits",(err,visits)=>{
+      process.exit(0)
+      client.get("visits",(err,visits)=>{
         res.send(visits)
         client.set("visits",parseInt(visits)+1)
     })
