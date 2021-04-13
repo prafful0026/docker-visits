b8f65a2588a1223f1531d84e8107c6fa4ec90261
prafful0026 <prafful0026@gmail.com>
2021-04-13 22:05:39 +0530

    connected two saperate containers using docker compose

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require ("express")
+const redis=require("redis")
+
+const app=express()
+const client=redis.createClient({
+    host: "redis-server"
+})
+
+client.set("visits",0)
+
+
+app.get("/",(req,res)=>{
+
+    client.get("visits",(err,visits)=>{
+        res.send(visits)
+        client.set("visits",parseInt(visits)+1)
+    })
+})
+
+app.listen(8080,()=>{
+console.log("on 8080")
+})
